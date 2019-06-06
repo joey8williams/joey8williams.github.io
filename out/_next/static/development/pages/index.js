aec13771b55c866a5bd4450ef0b6aabf5303af26
Roodey <joey8williams@gmail.com>
2019-06-06 12:10:41 -0400

    Deploy Next.js to gh-pages

--- out/_next/static/development/pages/index.js
+++ out/_next/static/development/pages/index.js
@@ -126,7 +126,7 @@ var Header = function Header() {
     },
     __self: this
   }, "Home")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
-    href: "/Resume",
+    href: "/Projects",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 28
@@ -138,12 +138,25 @@ var Header = function Header() {
       lineNumber: 29
     },
     __self: this
+  }, "Projects")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/Resume",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
   }, "Resume")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
     href: "https://github.com/joey8williams",
     target: "_blank",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 31
+      lineNumber: 34
     },
     __self: this
   }, "Github"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
@@ -151,7 +164,7 @@ var Header = function Header() {
     target: "blank",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 34
+      lineNumber: 37
     },
     __self: this
   }, "LinkedIn")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(LogoLanding, {
@@ -159,7 +172,7 @@ var Header = function Header() {
     alt: "Image",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 39
+      lineNumber: 42
     },
     __self: this
   }));
@@ -12971,7 +12984,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_Paragraph__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../styles/Paragraph */ "./styles/Paragraph.js");
 /* harmony import */ var _styles_MainHeading__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../styles/MainHeading */ "./styles/MainHeading.js");
 /* harmony import */ var _styles_Article__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../styles/Article */ "./styles/Article.js");
+/* harmony import */ var _styles_Todo__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../styles/Todo */ "./styles/Todo.js");
+/* harmony import */ var _styles_Link__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../styles/Link */ "./styles/Link.js");
 var _jsxFileName = "/Users/Roodey/Documents/Projects/Portfolio/web/pages/index.js";
+
+
 
 
 
@@ -12982,28 +12999,60 @@ var Index = function Index() {
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Layout__WEBPACK_IMPORTED_MODULE_1__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
+      lineNumber: 9
     },
     __self: this
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Article__WEBPACK_IMPORTED_MODULE_4__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 8
+      lineNumber: 10
     },
     __self: this
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_MainHeading__WEBPACK_IMPORTED_MODULE_3__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 9
+      lineNumber: 11
     },
     __self: this
-  }, "Thanks for Stopping by"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Paragraph__WEBPACK_IMPORTED_MODULE_2__["default"], {
+  }, "About the Author"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Paragraph__WEBPACK_IMPORTED_MODULE_2__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 10
+      lineNumber: 12
     },
     __self: this
-  }, "Thank you for taking a moment to browse my site. If there are any questions I can answer for you, please don't hesitate to reach out via the \"contact\" button on the right side of the page (TODO).")));
+  }, "Thank you for taking a moment to browse my site. If there are any questions I can answer for you, please don't hesitate to reach out via the \"contact\" button on the right side of the page ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Todo__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }), ". This page is an introduction to who I am, if you are looking for projects or work please use the menu above to find my Resume or Github."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Paragraph__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "My name is Joey Williams. I work in the field of software development with a focus on front end web development. My experience programming and design began when I was about 12, working with lua scripting as well as building static sites for fun. Growing up in small town Kentucky, there wasn't a ton of opportunity for learning how to program or design unless you had the desire to teach yourself. I always had viewed it as a hobby, and enjoyed math and physics more, so I began my college career as a Mechanical Engineer at University of Kentucky. I left most of my programming behind and focused entirely on developing my skills as an engineer until the end of my third year. There was a required course for MATLab, and working in MATLab sparked my desire for development again. The following semester, I switched my major to Computer Science, found", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    href: "https://awesomeinc.org",
+    target: "_blank",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, "Awesome Inc "), "who was willing to hire me to build static sites (via jekyll, WordPress, Shopify, and raw html/css/js) as well as some web apps (via EmberJs, ExpressJs). Working in those languages, while also learning Java, C, and SQL in school taught me that an expert level of knowledge with a language is helpful, but not nearly as much as having a solid approach to solving problems. This helped greatly in my transition to my development positions post college."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Paragraph__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }, "Upon graduating I had received an offer to work for the Kentucky Transportation Department (KYTC) as a full-stack web developer, in a Microsoft stack (SqlServer, C#/.NET, JQuery, Bootstrap). Being at KYTC was my first experience working with large code bases that I didn't write, and having to make changes to that code that didn't break the application. This quickly brought about the lesson of writing code that scales. The more I could refactor and reuse code, and the more it could be decoupled (within reason) the easier changes down the road became. The value of spending the extra time up front desigining, reworking, and implementing these designs massively cut down on the time it took to fix bugs, as well as extend the application. Since we worked with JQuery, design patterns were left more or less up to developer writing the application. I enjoyed the flexibility to experiment, and it helped when I moved into modern (NodeJs) front end tooling and frameworks. Today I am seeking work as a React Developer, or as a Phoenix/Elixir developer, ideally both."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_Paragraph__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56
+    },
+    __self: this
+  }, "Outside of work I enjoy traveling, learning about other cultures, kayaking, and music. I believe new experiences are the key to growth, and exploration/experimentation is the only way to have those new experiences. If you would like to know more please feel free to reach out or continue browsing the site. Thank you and enjoy!")));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Index);
@@ -13109,6 +13158,27 @@ var Paragraph = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].p.with
 
 /***/ }),
 
+/***/ "./styles/Todo.js":
+/*!************************!*\
+  !*** ./styles/Todo.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./theme */ "./styles/theme.js");
+
+
+var Todo = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].span.withConfig({
+  displayName: "Todo",
+  componentId: "sc-1k0kmkv-0"
+})(["&::before{content:\"TODO\";font-family:", ";font-size:32px;color:red;}"], _theme__WEBPACK_IMPORTED_MODULE_1__["default"].font);
+/* harmony default export */ __webpack_exports__["default"] = (Todo);
+
+/***/ }),
+
 /***/ "./styles/theme.js":
 /*!*************************!*\
   !*** ./styles/theme.js ***!
@@ -13140,6 +13210,8 @@ var theme = {
   black: '#403D4B',
   white: "#FDFDFE",
   primary: "rgb(23,58,122)",
+  //Fonts
+  font: "Titillium Web, sans-serif",
   //Z index levels
   l0: 0,
   l1: 1,
@@ -13151,7 +13223,7 @@ var theme = {
 
 /***/ }),
 
-/***/ 7:
+/***/ 0:
 /*!**********************************************************************************************************************************************!*\
   !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2FRoodey%2FDocuments%2FProjects%2FPortfolio%2Fweb%2Fpages%2Findex.js ***!
   \**********************************************************************************************************************************************/
@@ -13174,5 +13246,5 @@ module.exports = dll_6dc2816e14fab51b8269;
 
 /***/ })
 
-},[[7,"static/runtime/webpack.js","styles"]]]);
+},[[0,"static/runtime/webpack.js","styles"]]]);
 //# sourceMappingURL=index.js.map
