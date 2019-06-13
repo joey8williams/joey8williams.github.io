7883e7f80b54dbcf77f002d6116f444d0416ca45
Roodey <joey8williams@gmail.com>
2019-06-13 13:02:49 -0400

    New Deployment

---
+++ out/_next/static/webpack/static/development/pages/Resume.js.67e605211051156210f7.hot-update.js
@@ -0,0 +1,29 @@
+webpackHotUpdate("static/development/pages/Resume.js",{
+
+/***/ "./styles/Paragraph.js":
+/*!*****************************!*\
+  !*** ./styles/Paragraph.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./theme */ "./styles/theme.js");
+
+
+var Paragraph = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].p.withConfig({
+  displayName: "Paragraph",
+  componentId: "sc-9t330a-0"
+})(["font-size:", ";margin-top:15px;color:", ";text-align:", ";"], _theme__WEBPACK_IMPORTED_MODULE_1__["default"].f0, function (props) {
+  return props.white ? _theme__WEBPACK_IMPORTED_MODULE_1__["default"].white : _theme__WEBPACK_IMPORTED_MODULE_1__["default"].black;
+}, function (props) {
+  return props.center ? "center" : "left";
+});
+/* harmony default export */ __webpack_exports__["default"] = (Paragraph);
+
+/***/ })
+
+})
+//# sourceMappingURL=Resume.js.67e605211051156210f7.hot-update.js.map
