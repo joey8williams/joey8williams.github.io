7ee1bf6f55dc6e3d70d1f08d7e03bac1cdcee456
Roodey <joey8williams@gmail.com>
2019-06-14 08:16:48 -0400

    New Deployment

---
+++ out/_next/static/webpack/static/development/pages/About.js.0fc3738a475912f4f710.hot-update.js
@@ -0,0 +1,31 @@
+webpackHotUpdate("static/development/pages/About.js",{
+
+/***/ "./styles/MainHeading.js":
+/*!*******************************!*\
+  !*** ./styles/MainHeading.js ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./theme */ "./styles/theme.js");
+
+
+var MainHeading = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].h1.withConfig({
+  displayName: "MainHeading",
+  componentId: "inywhp-0"
+})(["color:", ";margin:0;font-size:", ";text-align:", ";"], function (props) {
+  return props.primary ? _theme__WEBPACK_IMPORTED_MODULE_1__["default"].primary : _theme__WEBPACK_IMPORTED_MODULE_1__["default"].white;
+}, function (props) {
+  return props.bigger ? "3rem" : "2.5rem";
+}, function (props) {
+  return props.center ? "center" : "left";
+});
+/* harmony default export */ __webpack_exports__["default"] = (MainHeading);
+
+/***/ })
+
+})
+//# sourceMappingURL=About.js.0fc3738a475912f4f710.hot-update.js.map
